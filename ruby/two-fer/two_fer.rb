f8557dc6e39b0e3140673079ce467037b02e6d7d
Yusaira Khan <yusairak@nulogy.com>
2021-03-18 22:03:12 -0400

    finish twofer

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,8 +1,6 @@
-=begin
-Write your code for the 'Two Fer' exercise in this file. Make the tests in
-`two_fer_test.rb` pass.
+class TwoFer
+  def self.two_fer (name="you")
+    "One for #{name}, one for me."
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/two-fer` directory.
-=end
-
+end
