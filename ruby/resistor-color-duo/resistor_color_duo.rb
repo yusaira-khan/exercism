d8970ce074b723fbb366b795e411372f17a48395
Yusaira Khan <yusairak@nulogy.com>
2021-03-19 11:53:36 -0400

    finish splitting color lsit

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,9 +1,15 @@
 class ResistorColorDuo
-  def self.colorsString
+  def self.colorDashes
     "Black - Brown - Red - Orange - Yellow - Green - Blue - Violet - Grey - White"
     #0-9
   end
-  def self.value
+  def self.colorStr
+    self.colorDashes.gsub("- ","").downcase
+  end
+  def self.colorList
+    self.colorStr.split(" ")
+  end
+  def self.value l
   end
 
 end
--- ruby/resistor-color-duo/resistor_color_duo_test.rb
+++ ruby/resistor-color-duo/resistor_color_duo_test.rb
@@ -1,14 +1,19 @@
 require 'minitest/autorun'
 require_relative 'resistor_color_duo'
-
 # Common test data version: 2.1.0 00dda3a
 
 def assert_start_with full, sub
   assert (full.start_with? sub) , ("Expected #{full.inspect} to start with #{sub.inspect}")
 end
 class ResistorColorDuoTest < Minitest::Test
-  def test_colors
-    assert_start_with ResistorColorDuo.colorsString ,"Black - "
+  def test_colorstr
+    assert_start_with ResistorColorDuo.colorStr, "black brown"
+  end
+  def test_colorsplit
+    assert_equal  "black", ResistorColorDuo.colorList[0]
+  end
+  def test_colordashes
+    assert_start_with ResistorColorDuo.colorDashes, "Black - "
   end
 
   def test_brown_and_black
