14ed0b7eeeb241a93e93da505d6c3caf1752e66d
Yusaira Khan <yusairak@nulogy.com>
2021-04-01 12:18:37 -0400

    finish spaceage

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,3 +1,19 @@
 package space
 
 type Planet string
+var years = map[Planet]float64  {
+	"Mercury":0.2408467,
+	"Venus":0.61519726,
+	"Earth":1.0,
+	"Mars":1.8808158,
+	"Jupiter":11.862615,
+	"Saturn":29.447498,
+	"Uranus":84.016846,
+	"Neptune":164.79132,
+}
+var earthSeconds float64 = 31557600
+
+func Age(seconds float64, planet Planet) float64  {
+	yearSeconds := years[planet] * earthSeconds
+	return seconds/yearSeconds
+}
