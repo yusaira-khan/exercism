a7615f5901f32f377b7fd1bf5f25e39212277bac
Yusaira Khan <yusairak@nulogy.com>
2021-04-01 16:13:35 -0400

    finish hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,12 +1,20 @@
 package hamming
 
+import "fmt"
+
 func Distance(a, b string) (int, error) {
 	var err error = nil
 	var dis int = 0
-	if len(a) == 1 {
-		if a[0] != b[0] {
-			dis = 1
+	if len(a) != len(b) {
+		return 0, fmt.Errorf("length mismatch")
+	}
+	for i, _ := range a{
+		elemA := a[i]
+		elemB := b[i]
+		if elemA != elemB {
+			dis++
 		}
+		i++
 	}
 	return dis, err
 }
