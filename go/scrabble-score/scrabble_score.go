af26155c0f6ef567d8b4147d86e2c3b5f6580150
Yusaira Khan <yusairak@nulogy.com>
2021-04-02 14:08:30 -0400

    add thing to turn into dictionary

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,5 +1,12 @@
 package scrabble
 
+"A, E, I, O, U, L, N, R, S, T"      1
+"D, G"                               2
+"B, C, M, P"                         3
+"F, H, V, W, Y"                       4
+"K"                                  5
+"J, X"                               8
+"Q, Z"                               10
 func Score(letter string) int{
 	return 1
 }
