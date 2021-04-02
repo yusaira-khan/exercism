67b817e8ea5850871e06484eedeafd98539d2be8
Yusaira Khan <yusairak@nulogy.com>
2021-04-01 20:04:34 -0400

    finish raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,1 +1,21 @@
 package raindrops
+
+import "fmt"
+
+func Convert(num int) string {
+	sound := ""
+	if (num%3 ==0){
+		sound += "Pling"
+	}
+	if (num%5 ==0){
+		sound += "Plang"
+	}
+	if (num%7 ==0){
+		sound += "Plong"
+	}
+	if (sound == ""){
+		sound = fmt.Sprint(num)
+	}
+
+	return sound
+}
