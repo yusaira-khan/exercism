08795d477a4985d5a6ad215e7741d9231ea8375c
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2022-10-29 17:27:35 -0400

    finishes rust 3

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -2,10 +2,19 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
+fn error_rate(speed: u8) -> f64{
+    match speed {
+        1..=4 => 1.0,
+        5..=8 => 0.9,
+        9 | 10 => 0.77,
+        _ => 0.0
+    }
+}
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let base_rate :f64 = 221.0;
+    error_rate(speed) * base_rate * (speed as f64)
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed)/60.0)as u32
 }
