95fa7275b8366e76acbec76b29e91df013b785c7
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2022-11-13 18:32:24 -0500

    4

--- rust/semi-structured-logs/src/lib.rs
+++ rust/semi-structured-logs/src/lib.rs
@@ -5,20 +5,22 @@
 /// various log levels
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum LogLevel {
+    Debug,
     Info,
     Warning,
     Error,
 }
 /// primary function for emitting logs
 pub fn log(level: LogLevel, message: &str) -> String {
-    unimplemented!("return a message for the given log level")
+    let level_str = format!("{:?}",level).to_uppercase();
+    format!("[{}]: {}", level_str, message)
 }
 pub fn info(message: &str) -> String {
-    unimplemented!("return a message for info log level")
+    log(LogLevel::Info,message)
 }
 pub fn warn(message: &str) -> String {
-    unimplemented!("return a message for warn log level")
+    log(LogLevel::Warning,message)
 }
 pub fn error(message: &str) -> String {
-    unimplemented!("return a message for error log level")
+    log(LogLevel::Error,message)
 }
